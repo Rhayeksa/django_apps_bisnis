8788ccbb7315855d4c3f11d6a5163d993b73f457
rhayeksa <rhayeksads@gmail.com>
2023-09-14 11:44:49 +0700

    initial unit test

--- apps/kontak/tests.py
+++ apps/kontak/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class AsdfTestCase(TestCase):
+    def test_asdf(self):
+        self.assertEqual(11, 5+5)
+
+    def test_asdf1(self):
+        self.assertEqual(11, 5+5)
+
+    def test_asdf2(self):
+        self.assertEqual(11, 5+5)
+
+# note: penamaan class bebas, dan penamaan methode dalam class harus di awali test_
